7932b53a8ad1f52ecebe6d9e080edc3bb5191035
Zygmunt Krawiec <zygmuntopolis@o2.pl>
2018-03-30 22:15:56 +0200

    Added LicensesDataBinder helper class in presentation layer

---
+++ LicenseManagementSystemPresentationLayer/Code/LicensesDataBinder.cs
@@ -0,0 +1,104 @@
+﻿using LicenseManagementSystemPresentationLayer.LicenseManagementSystemWebService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LicenseManagementSystemPresentationLayer.Code
+{
+
+    public class LicensesDataBinder
+    {
+        GridView gridView;
+        Repeater repeater;
+        DropDownList dropDownList;
+
+        public LicensesDataBinder(GridView gridView, Repeater repeater, DropDownList dropDownList)
+        {
+            this.gridView = gridView;
+            this.repeater = repeater;
+            this.dropDownList = dropDownList;
+        }
+
+        // Instance        
+
+        public void LicensesDataBind(LicenseManagementSystemWebServiceSoapClient wsClient, User user, int pageNumber, int columnToSort, bool typeOfSorting, int rowsOnPage)
+        {
+            // Get Licenses data from a database.
+            LicensesContainer result = wsClient.GetLicensesData(user.UserName, user.UserAccessNumber, pageNumber, columnToSort, rowsOnPage, typeOfSorting);
+
+            // Set number of rows to display in gridview
+            gridView.PageSize = rowsOnPage;
+
+            // Bind the data                   
+            gridView.DataSource = result.LicensesDataSet;
+            gridView.DataBind();
+
+            // Bind a page numbers.
+            repeaterDataBind(pageNumber, result.NumberOfAllLicenses, int.Parse(dropDownList.SelectedItem.Value));
+        }
+
+        private void repeaterDataBind(int pageNumber, int numberOfAllRows, int numberOfRowsPerPage)
+        {
+            // Create a container for page numbers
+            List<ListItem> pages = new List<ListItem>();
+
+            // Create containers to keep temp data for loops.
+            int i = 0, allPages = 0;
+
+            // If number o all rows in a data table are greater than number of rows displays on a single page,
+            // then calculate a number of all pages.
+            if (numberOfAllRows >= numberOfRowsPerPage)
+            {
+                allPages = (numberOfAllRows % numberOfRowsPerPage) == 0 ? (numberOfAllRows / numberOfRowsPerPage) : (numberOfAllRows / numberOfRowsPerPage) + 1;
+
+                // Create and add all calculated pages into the container.
+                do
+                {
+                    // Selected property is used to set visibility of the number on a web page.
+                    pages.Add(new ListItem() { Value = i.ToString(), Text = (i + 1).ToString(), Enabled = i != pageNumber, Selected = true });
+                    i++;
+                } while (i < allPages);
+            }
+            else
+            {
+                // if number of rows on a page are greater than number of rows in a table then add only one page and set it visibility to false.
+                pages.Add(new ListItem() { Value = i.ToString(), Text = (i + 1).ToString(), Enabled = i != pageNumber, Selected = false });
+            }
+
+            // Bind the container to the repeater.
+            repeater.DataSource = pages;
+            repeater.DataBind();
+        }
+
+        // Static
+        /// <summary>
+        /// Finds an active licenses page number from a Repeater web control.
+        /// </summary>
+        /// <param name="rprPages">Repeater with LinkButtons to represent a page numbers.</param>
+        /// <returns>Number of active page.</returns>
+        public static int GetCurrentPageNumber(Repeater rprPages)
+        {
+            // Take current page number.
+            var pageNumberCollection = rprPages.Items.OfType<RepeaterItem>().ToList().Select(r => r.Controls);
+
+            // This part below need to be redesign into LINQ sequence            
+            int currentPageNumber = -1;
+            foreach (ControlCollection cc in pageNumberCollection)
+            {
+                foreach (Control c in cc)
+                {
+                    if (c is LinkButton)
+                    {
+                        if (currentPageNumber != -1)
+                            break;
+                        currentPageNumber = ((LinkButton)c).Enabled == false ? int.Parse(((LinkButton)c).CommandArgument) : -1;
+                    }
+                }
+            }
+            return currentPageNumber;
+        }
+    }
+}
--- LicenseManagementSystemPresentationLayer/Welcome.aspx.cs
+++ LicenseManagementSystemPresentationLayer/Welcome.aspx.cs
@@ -8,6 +8,7 @@ using System.Web.UI.WebControls;
 using LicenseManagementSystemPresentationLayer.LicenseManagementSystemWebService;
 using System.Data.SqlClient;
 using System.Data;
+using LicenseManagementSystemPresentationLayer.Code;
 
 
 namespace LicenseManagementSystemPresentationLayer
@@ -17,10 +18,16 @@ namespace LicenseManagementSystemPresentationLayer
         // WebService Client instance.
         LicenseManagementSystemWebServiceSoapClient wsClient = new LicenseManagementSystemWebServiceSoapClient();
 
+        // Helper class 
+        LicensesDataBinder licenseDataBinder;
+
         // Events
 
         protected void Page_Load(object sender, EventArgs e)
         {
+            // Create data binder
+            licenseDataBinder = new LicensesDataBinder(gvLicenseData, rprPages, ddlRowsPerPage);
+
             if (!Page.IsPostBack)
             {
                 // Display a name of logged user on page.
@@ -30,14 +37,14 @@ namespace LicenseManagementSystemPresentationLayer
                 throw new Exception("Uncomment the line above and a line in an authorization tag in web.config, change userdata in the line 136, and change hadr Guid into viewstateguid ");
 #endif
                 // Read a first portion of data and display it in a gridview.
-                licensesDataBind(0, 0, true, 10);
+                User user = new User() { UserName = "DupaEmail4", UserAccessNumber = Guid.Parse("d2d647d0-dfbd-40c2-a372-c14f6b88bf5a") };
+                licenseDataBinder.LicensesDataBind(wsClient, user, 0, 0, true, 10);
 
                 // Save an index of sorted column.
                 ViewState["indexSortedColumn"] = 0;
 
                 // Save a sort direction (ASC - true, DESC - false).
                 ViewState["sortDirection"] = false;
-
             }
         }
 
@@ -56,7 +63,7 @@ namespace LicenseManagementSystemPresentationLayer
         protected void grvLicenseData_Sorting(object sender, GridViewSortEventArgs e)
         {
             // Take current page number.
-            int currentPageNumber = getCurrentPageNumber();
+            int currentPageNumber = LicensesDataBinder.GetCurrentPageNumber(rprPages);
 
             // Take index of column to sort.      
             TableCell x1 = ((GridView)sender).HeaderRow.Cells.OfType<TableCell>().Where(t => t.Controls.OfType<LinkButton>().Any(l => l.Text == e.SortExpression)).First();
@@ -68,8 +75,9 @@ namespace LicenseManagementSystemPresentationLayer
             // From a drope down list take a number of rows to display in a gridview.
             int rows = int.Parse(ddlRowsPerPage.SelectedItem.Value);
 
-            // Choose a sorting direction (if true then ASC if false then DESC).           
-            licensesDataBind(currentPageNumber, columnIndex, (bool)ViewState["sortDirection"], rows);
+            // Choose a sorting direction (if true then ASC if false then DESC).
+            User user = new User() { UserName = "DupaEmail4", UserAccessNumber = Guid.Parse("d2d647d0-dfbd-40c2-a372-c14f6b88bf5a") };
+            licenseDataBinder.LicensesDataBind(wsClient, user, currentPageNumber, columnIndex, (bool)ViewState["sortDirection"], rows);
             ViewState["sortDirection"] = !(bool)ViewState["sortDirection"];
         }
 
@@ -82,7 +90,8 @@ namespace LicenseManagementSystemPresentationLayer
             int rows = int.Parse(ddlRowsPerPage.SelectedItem.Value);
 
             // Bind a chosen portion of data to the gridview
-            licensesDataBind(pageNumber, (int)ViewState["indexSortedColumn"], !(bool)ViewState["sortDirection"], rows);
+            User user = new User() { UserName = "DupaEmail4", UserAccessNumber = Guid.Parse("d2d647d0-dfbd-40c2-a372-c14f6b88bf5a") };
+            licenseDataBinder.LicensesDataBind(wsClient, user, pageNumber, (int)ViewState["indexSortedColumn"], !(bool)ViewState["sortDirection"], rows);
 
             // Clear all texboxes
             txtUserEmal.Text = string.Empty;
@@ -96,7 +105,8 @@ namespace LicenseManagementSystemPresentationLayer
             int rows = int.Parse(((DropDownList)sender).SelectedItem.Value);
 
             // Bind a chosen portion of data to the gridview
-            licensesDataBind(0, (int)ViewState["indexSortedColumn"], true, rows);
+            User user = new User() { UserName = "DupaEmail4", UserAccessNumber = Guid.Parse("d2d647d0-dfbd-40c2-a372-c14f6b88bf5a") };
+            licenseDataBinder.LicensesDataBind(wsClient, user, 0, (int)ViewState["indexSortedColumn"], true, rows);
             ViewState["sortDirection"] = false;
 
             // Clear all texboxes
@@ -162,7 +172,7 @@ namespace LicenseManagementSystemPresentationLayer
 
         protected void btnModify_Click(object sender, EventArgs e)
         {
-            // TODO: Check whats happen when js is disabled on client site
+            // TODO: Check whats happen if js is disabled on client side
             bool result = false;
             if (gvLicenseData.Rows.OfType<GridViewRow>().Any(x => x.Attributes["Style"] == "Background:Green"))
             {
@@ -183,81 +193,5 @@ namespace LicenseManagementSystemPresentationLayer
             lblMessages.Text = result ? "User license data was modyfied." : $"User with {txtUserName.Text} name and {txtUserEmal.Text} email could not be modyfied.";
 
         }
-
-        // Helper methods - to redesin into another class.
-
-        protected void dataBindRepeater(int pageNumber, int numberOfAllRows, int numberOfRowsPerPage)
-        {
-            // Create a container for page numbers
-            List<ListItem> pages = new List<ListItem>();
-
-            // Create containers to keep temp data for loops.
-            int i = 0, allPages = 0;
-
-            // If number o all rows in a data table are greater than number of rows displays on a single page,
-            // then calculate a number of all pages.
-            if (numberOfAllRows >= numberOfRowsPerPage)
-            {
-                allPages = (numberOfAllRows % numberOfRowsPerPage) == 0 ? (numberOfAllRows / numberOfRowsPerPage) : (numberOfAllRows / numberOfRowsPerPage) + 1;
-
-                // Create and add all calculated pages into the container.
-                do
-                {
-                    // Selected property is used to set visibility of the number on a web page.
-                    pages.Add(new ListItem() { Value = i.ToString(), Text = (i + 1).ToString(), Enabled = i != pageNumber, Selected = true });
-                    i++;
-                } while (i < allPages);
-            }
-            else
-            {
-                // if number of rows on a page are greater than number of rows in a table then add only one page and set it visibility to false.
-                pages.Add(new ListItem() { Value = i.ToString(), Text = (i + 1).ToString(), Enabled = i != pageNumber, Selected = false });
-            }
-
-
-
-            // Bind the container to the repeater.
-            rprPages.DataSource = pages;
-            rprPages.DataBind();
-        }
-
-        protected void licensesDataBind(int pageNumber, int columnToSort, bool typeOfSorting, int rowsOnPage)
-        {
-            // Get Licenses data from a database.
-            LicensesContainer result = wsClient.GetLicensesData("DupaEmail4", Guid.Parse("d2d647d0-dfbd-40c2-a372-c14f6b88bf5a"), pageNumber, columnToSort, rowsOnPage, typeOfSorting);
-
-            // Set number of rows to display in gridview
-            gvLicenseData.PageSize = rowsOnPage;
-
-            // Bind the data                   
-            gvLicenseData.DataSource = result.LicensesDataSet;
-            gvLicenseData.DataBind();
-
-            // Bind a page numbers.
-            dataBindRepeater(pageNumber, result.NumberOfAllLicenses, int.Parse(ddlRowsPerPage.SelectedItem.Value));
-        }
-
-        protected int getCurrentPageNumber()
-        {
-            // Take current page number.
-            var pageNumberCollection = rprPages.Items.OfType<RepeaterItem>().ToList().Select(r => r.Controls);
-            // This part below need to be redesign into LINQ sequence            
-            int currentPageNumber = -1;
-            foreach (ControlCollection cc in pageNumberCollection)
-            {
-                foreach (Control c in cc)
-                {
-                    if (c is LinkButton)
-                    {
-                        if (currentPageNumber != -1)
-                            break;
-                        currentPageNumber = ((LinkButton)c).Enabled == false ? int.Parse(((LinkButton)c).CommandArgument) : -1;
-                    }
-                }
-            }
-            return currentPageNumber;
-        }
-
-
     }
 }
